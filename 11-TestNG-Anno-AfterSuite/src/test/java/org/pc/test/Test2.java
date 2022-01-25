a4549f065980eb9751c5c837f0550a4f59bf9099
Admin <Admin@DESKTOP-6NCKN49>
2022-01-25 20:41:39 +0530

    @AfterSuite Annotation

---
+++ 11-TestNG-Anno-AfterSuite/src/test/java/org/pc/test/Test2.java
@@ -0,0 +1,22 @@
+package org.pc.test;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void test1() {
+		System.out.println("Test 2 Class Test Method 1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test 2 Class Test Method 2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Test 2 Class Test Method 3");
+	}
+
+}
